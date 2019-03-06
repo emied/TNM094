79f36be47ccf3063a3d215bfb71d735cb6b167e9
evelynbankell <evelyn@bankell.se>
2019-03-06 15:49:14 +0100

    Added average velocity to box 2

--- express-dashboard-server/public/javascripts/info-boxes.js
+++ express-dashboard-server/public/javascripts/info-boxes.js
@@ -2,28 +2,23 @@ function infoBox(data) {
 
   var cross_filter = crossfilter(data);
 
-  var test = cross_filter.dimension(function(d) {
-    var distance = d.distance;
-    return distance;
-  });
-
   var distSum = 0;
   data.forEach(function(d) {
     distSum += +d.distance;
-
-
   });
 
+  var avgVel = 0;
+  var counter = 0;
+  data.forEach(function(d) {
+    avgVel += +d.speed;
+    ++counter
+  });
+  avgVel = (avgVel/counter).toFixed(2);
 
-  console.log(distSum);
 
 
+	document.getElementById('info-box-1').innerHTML = "<h5>" + Math.round(distSum)/1000 + " km" + "</h5><h4>" + "<br>" + "Total Distance" + "</h4>" ;
 
-
-
-
-
-	document.getElementById('info-box-1').innerHTML = "<h5>" + Math.round(distSum)/1000 + " km" + "</h5><h4>" + "<br>" + "Total distance" + "</h4>" ;
-
+  document.getElementById('info-box-2').innerHTML = "<h5>" + avgVel + " km/h" + "</h5><h4>" + "<br>" + "Average Velocity" + "</h4>" ;
 
 }
