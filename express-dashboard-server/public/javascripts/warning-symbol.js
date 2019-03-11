f659a148b80f41dabdd37c705da794d4c878d4cb
Ericamedx <32699494+Ericamedx@users.noreply.github.com>
2019-03-11 14:57:24 +0100

    added a dropdown meny + popup

---
+++ express-dashboard-server/public/javascripts/warning-symbol.js
@@ -0,0 +1,19 @@
+/*****************
+varning_symbol.js
+
+Created an icon for the warning dashboard.
+When the icon is hovered over you will get
+a short review, eg. 10 warning and when the icon is
+clicked a new (pop-up?)window will be opened
+
+TODO
+make it "blink" when a warning apears
+*****************/
+var open = true;
+var modal = document.getElementById('warModal');
+
+function toggleWar() { //Open small window
+    document.getElementById("meny-warning");
+    modal.style.display = "block";
+    open = !open;
+}
