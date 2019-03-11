c9f2759b96c5ccc62d4437efde2518b02252b957
Linus Mossberg <linmo400@student.liu.se>
2019-03-11 19:56:05 +0100

    Add station dot animation based on crossfilter

--- express-dashboard-server/public/javascripts/draw-chart.js
+++ express-dashboard-server/public/javascripts/draw-chart.js
@@ -72,60 +72,113 @@ function drawChart(data) {
 
 			Based on https://stackoverflow.com/a/35476690 
 			**********************************************/
-			var projection; // this gets set later.
+			// var projection; // this gets set later.
+			// function drawStationDots(_chart, selection) 
+			// {
+
+			// 	var svg = _chart.svg();
+
+			// 	var group = svg.selectAll("g.station_dots");
+
+			// 	group.each(function(d,i) {
+			// 		console.log(this)
+			// 	});
+
+			// 	if (group.empty()) {
+			// 		group = svg.append("g").classed("station_dots", true);
+			// 	}
+
+			// 	var filtered_stations_map = new Map();
+
+			// 	_chart.dimension().top(Infinity).map(function(d) {
+			// 		var station = bike_stations.get(d.start_id);
+			// 		if(station)
+			// 		{
+			// 			filtered_stations_map.set(d.start_id, station);
+			// 		}
+			// 	});
+
+			// 	let filtered_stations = Array.from( filtered_stations_map.values() );
+
+			// 	var additional_nodes = group.selectAll("circle").data(filtered_stations, function(x) { return x.zip; });
+
+			// 	additional_nodes.enter()
+			// 		.append("circle")
+			// 		.attr("x", 0)
+			// 		.attr("y", 0)
+			// 		.attr("r", 3)
+			// 		.attr("transform", function(d){ var v = projection([d.lon, d.lat]); return "translate(" + v[0] + "," + v[1] + ")"; })
+			// 		.style("opacity", 0.0)
+			// 		.style("fill", "white")
+			// 		.style("stroke", "black")
+			// 		.style("stroke-width", "0.1%")
+			// 		.transition().style("opacity", 1.0).duration(500);
+
+			// 	additional_nodes.exit().remove();
+			// }
+
+			/********************************************** 
+			Function that draws circles at station coordinates
+			on the map chart. Called by the map chart each time
+			it re-renders (i.e. when crossfilter changes).
+			**********************************************/
+			var projection; // this gets set further down in the map chart section.
 			function drawStationDots(_chart, selection) 
 			{
 
-				// No need to redraw when filter changes because dots 
-				// don't depend on filters yet.
-				if(!d3.select("g.station_dots").empty())
-				{
-					//return;
-					//if(d3.select(".station-dots").style("display") == "none") { return; }
-					
-				}
-
 				var svg = _chart.svg();
-				svg.selectAll("g.station_dots").remove();
 
 				var group = svg.selectAll("g.station_dots");
 
+				// Create SVG circle elements for all stations.
+				// This is only done once when the page first loads.
 				if (group.empty()) {
+
 					group = svg.append("g").classed("station_dots", true);
+
+					var additional_nodes = group.selectAll("circle").data(bike_stations_zip, function(x) { return x.id; });
+
+					additional_nodes.enter()
+						.append("circle")
+						.attr("x", 0)
+						.attr("y", 0)
+						.attr("r", 0)
+						.attr("transform", function(d){ var v = projection([d.lon, d.lat]); return "translate(" + v[0] + "," + v[1] + ")"; })
+						.style("opacity", 1.0)
+						.style("fill", "white")
+						.style("stroke", "black")
+						.style("stroke-width", "0.1%")
+
+					additional_nodes.exit().remove();
 				}
 
-				/* 
-				This function should probably filter the dots depending on current selection/filter, 
-				but it has to relate to the original bike dimension and not station coordinates.
-				Not sure how to implement this yet so for now the dots are not filtered.
-				*/
-
-				var filtered_stations_map = new Map();
+				// Find stations selected with current crossfilter
+				var filtered_stations = new Map();
 				_chart.dimension().top(Infinity).map(function(d) {
 					var station = bike_stations.get(d.start_id);
 					if(station)
 					{
-						filtered_stations_map.set(d.start_id, station);
+						filtered_stations.set(d.start_id, station);
 					}
 				});
 
-				let filtered_stations = Array.from( filtered_stations_map.values() );
-
-				var additional_nodes = group.selectAll("circle").data(filtered_stations, function(x) { return x.zip; });
-
-				additional_nodes.enter()
-					.append("circle")
-					.attr("x", 0)
-					.attr("y", 0)
-					.attr("r", 3)
-					.attr("transform", function(d){ var v = projection([d.lon, d.lat]); return "translate(" + v[0] + "," + v[1] + ")"; })
-					.style("opacity", 1.0)
-					.style("fill", "white")
-					.style("stroke", "black")
-					.style("stroke-width", "0.1%")
-					//.transition().style("opacity", 1.0).duration(500);
-
-				additional_nodes.exit().remove();
+				// Loop through all SVG circle elements and set radius
+				// to 3 or 0 depending on if the station should be visible
+				// or not according to the filtered stations.
+				// Also uses a variable transition duration to make
+				// the radius animate in and out at different speeds.
+				group.selectAll("circle").each(function(d, i) {
+					if(filtered_stations.get(d.id))
+					{
+						d3.select(this)
+							.transition().attr("r", 3.5).duration(i*10)
+					}
+					else
+					{
+						d3.select(this)
+							.transition().attr("r", 0).duration(i*10)
+					}
+				});
 			}
 
 			var cross_filter = crossfilter(data);
