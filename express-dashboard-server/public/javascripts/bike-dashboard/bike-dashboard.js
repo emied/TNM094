2383b8f3774a4eb0f5f9c3fcd3dac3aa9a68d882
Linus Mossberg <linmo400@student.liu.se>
2019-03-27 14:23:48 +0100

    Add date chart class

--- express-dashboard-server/public/javascripts/bike-dashboard/bike-dashboard.js
+++ express-dashboard-server/public/javascripts/bike-dashboard/bike-dashboard.js
@@ -3,13 +3,16 @@ class BikeDashboard
 	constructor(data)
 	{
 		this.cross_filter = crossfilter(data);
+		
 		this.bike_id_chart = new BikeIdChart(this.cross_filter, 'bike-id-chart');
 		this.gender_chart = new GenderChart(this.cross_filter, 'pie-chart');
+		this.date_chart = new DateChart(this.cross_filter, 'date-bar-chart');
 	}
 
 	resize()
 	{
 		this.bike_id_chart.resize();
 		this.gender_chart.resize();
+		this.date_chart.resize();
 	}
 }
