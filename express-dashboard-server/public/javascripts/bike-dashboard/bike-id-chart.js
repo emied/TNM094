9b23eff42068aaec96eb1f52018ad37f46f59de2
Linus Mossberg <linmo400@student.liu.se>
2019-03-27 11:15:37 +0100

    Add bike ID chart class

---
+++ express-dashboard-server/public/javascripts/bike-dashboard/bike-id-chart.js
@@ -0,0 +1,36 @@
+class BikeIdChart
+{
+	constructor(cross_filter, container_id)
+	{
+		this.container_id = '#' + container_id;
+		this.chart = dc.rowChart(this.container_id);
+		this.dimension = cross_filter.dimension( d => { return d.bike_id; });
+		this.group = bike_id_dimension.group().reduceSum( d => { return d.distance; });
+
+		this.chart
+			.width($(this.container_id).width())
+			.height(240)
+			.group(this.group)
+			.dimension(this.dimension)
+			.margins({left: 30, top: 10, right: 50, bottom: -1}) // hide axes
+			.rowsCap(5)
+			.othersGrouper(false)
+			.label( d => { return 'Bike ID: ' + d.key + ", Distance: " + Math.round(d.value/1000.0) + " km"; })
+			.title( d => { return null; })
+			.elasticX(true)
+			.xAxis().ticks(3);
+
+		this.chart.render();
+	}
+
+	resize()
+	{
+		this.chart
+			.width($(this.container_id).width())
+			.transitionDuration(0);
+
+		this.chart.render();
+
+		this.chart.transitionDuration(750);
+	}
+}
