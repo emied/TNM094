ab91377c371cef6f7a42b7ceb8819f954ce7b6e8
evelynbankell <evelyn@bankell.se>
2019-04-03 15:08:42 +0200

    calculate warning humidity

--- express-dashboard-server/public/javascripts/warnings.js
+++ express-dashboard-server/public/javascripts/warnings.js
@@ -2,9 +2,38 @@ function warnings(data) {
 
   console.log(data.length);
 
-  for(i = 0; i < data.length; i++) {
+  var sum_humidity = 0;
 
+  for(i = 0; i < data.length; i++) {
+    data[i].humidity = parseFloat(data[i].humidity);
+    sum_humidity += data[i].humidity;
   }
+
+  var avg_humidity = sum_humidity/data.length;
+  console.log(avg_humidity);
+  var counter = 0;
+  var warningArray = new Array();
+
+
+  for(i = 0; i < data.length; i++) {
+    var value = parseFloat(data[i].humidity);
+
+    var diff_value = Math.abs(avg_humidity - value);
+    var deviation = diff_value/avg_humidity;
+
+    if(deviation > 0.2) {
+      warningArray[counter] = data[i];
+      ++counter;
+    }
+  }
+
+  const dataTable = $('#warnings').DataTable({
+    data: warningArray,
+    aoColumns: [
+      { title: 'Humidity', mData: 'humidity' }
+    ]
+
+  });
 
 
 }
