4e9b4fe2367de4becdbd35ddc4446dd8fc29ef41
Tim Cardell <timcardell@c-10-253-213-187.clients.eduroam.liu.se>
2019-04-11 15:37:32 +0200

    added rangechart

--- express-dashboard-server/public/javascripts/compressor-dashboard/charts/oil-pressure-chart.js
+++ express-dashboard-server/public/javascripts/compressor-dashboard/charts/oil-pressure-chart.js
@@ -4,6 +4,8 @@ export class OilPressureChart{
   constructor(cross_filter, container_id, height, start, end){
     this.container_id = container_id;
     this.chart = dc.lineChart(this.container_id);
+    this.rangeChart = dc.barChart('#range-chart')
+
 
     this.dimension = cross_filter.dimension(function(d) {
       var minute = new Date(d.start_time);
@@ -13,10 +15,22 @@ export class OilPressureChart{
 
     this.group = this.dimension.group().reduce(reduceAddAvg("oil_pressure"), reduceRemoveAvg("oil_pressure"), reduceInitAvg);
 
+    this.rangeChart
+      .width($(this.container_id).width())
+      .height(46)
+      .dimension(this.dimension)
+      .group(this.group)
+      .x(d3.scaleTime().domain([start, end]))
+      .centerBar(true)
+      .xUnits(d3.timeMonth)
+      .margins({left: 50, top: 0, right: 20, bottom: 20})
+      .render();
+
     this.chart
 			.width($(this.container_id).width())
 			.height(height)
 			.group(this.group)
+      .rangeChart(this.rangeChart)
 			.dimension(this.dimension)
 			.margins({left: 50, top: 20, right: 20, bottom: 20}) // hide axes
       .x(d3.scaleTime().domain([start, end]))
@@ -28,22 +42,33 @@ export class OilPressureChart{
       .zoomOutRestrict(true)
       .elasticY(true)
       .renderVerticalGridLines(true)
+      .renderHorizontalGridLines(true)
 			.valueAccessor(d => { return d.value.avg; })
 
     this.chart.render();
   	}
 
     resize(){
+      this.rangeChart
+        .width($(this.container_id).width())
+        .transitionDuration(0);
+
+      this.rangeChart.render();
+      this.rangeChart.transitionDuration(750);
+
       this.chart
         .width($(this.container_id).width())
         .transitionDuration(0);
 
       this.chart.render();
-
       this.chart.transitionDuration(750);
+
+
     }
 
     redraw(){
+      this.rangeChart.redraw();
       this.chart.redraw();
+
     }
 }
