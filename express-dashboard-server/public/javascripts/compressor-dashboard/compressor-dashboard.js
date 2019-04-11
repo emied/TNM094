6d223880dc99431a7f0aabd4c9b8ed3355d79012
Tim Cardell <timcardell@c-10-253-213-187.clients.eduroam.liu.se>
2019-04-11 12:59:45 +0200

    added classes to compressor datat

--- express-dashboard-server/public/javascripts/compressor-dashboard/compressor-dashboard.js
+++ express-dashboard-server/public/javascripts/compressor-dashboard/compressor-dashboard.js
@@ -3,6 +3,7 @@ import { AvgVibrationDisplay } from './displays/avg-vibration-display.js';
 import { AvgOilTempDisplay } from './displays/avg-oil-temp-display.js';
 import { AvgAmbTempDisplay } from './displays/avg-amb-temp-display.js';
 
+import { OilPressureChart } from './charts/oil-pressure-chart.js';
 
 export class CompressorDashboard {
 
@@ -10,11 +11,12 @@ export class CompressorDashboard {
 
     this.cross_filter = crossfilter(data);
 
-    this.avg_amb_temp_display = new AvgAmbTempDisplay(this.cross_filter, '#info-box-1');    
+    this.avg_amb_temp_display = new AvgAmbTempDisplay(this.cross_filter, '#info-box-1');
     this.avg_flow_display = new AvgFlowDisplay(this.cross_filter, '#info-box-2');
     this.avg_vibration_display = new AvgVibrationDisplay(this.cross_filter, '#info-box-3');
     this.avg_oil_temp_display = new AvgOilTempDisplay(this.cross_filter, '#info-box-4');
 
+    this.oil_pressure_chart = new OilPressureChart(this.cross_filter, '#line-chart-pressure')
 
   }
 }
