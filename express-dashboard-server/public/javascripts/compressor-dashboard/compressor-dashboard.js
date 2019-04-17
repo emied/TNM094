c92f29cd777f79389312dc922fb30f5df87cc052
evelynbankell <evelyn@bankell.se>
2019-04-17 10:33:47 +0200

    test

--- express-dashboard-server/public/javascripts/compressor-dashboard/compressor-dashboard.js
+++ express-dashboard-server/public/javascripts/compressor-dashboard/compressor-dashboard.js
@@ -28,8 +28,6 @@ export class CompressorDashboard {
     this.oil_pressure_chart = new MinuteLineChart(this.cross_filter, '#line-chart-pressure', 330, start, end, 'Oil Temp (°C)', 'oil_temp', this.range_chart.chart, this.dimension);
     this.flow_chart = new MinuteLineChart(this.cross_filter, '#line-chart-flow', 330, start, end, 'Oil Temp (°C)', 'flow', this.range_chart.chart, this.dimension);
 
-    this.range_chart.focusCharts([this.oil_pressure_chart,this.flow_chart]);
-
     this.range_chart.focusCharts = function (chartlist) {
       if (!arguments.length) {
           return this._focusCharts;
@@ -48,6 +46,8 @@ export class CompressorDashboard {
       });
       return this;
     };
+
+    this.range_chart.focusCharts([this.oil_pressure_chart,this.flow_chart]);
 
   }
 
