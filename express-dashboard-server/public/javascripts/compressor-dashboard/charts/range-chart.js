9f80c84e637fab2e64361199f85384d6363513f2
Tim Cardell <timcardell@c-10-253-213-187.clients.eduroam.liu.se>
2019-04-18 16:59:08 +0200

    added border and fixed averages

--- express-dashboard-server/public/javascripts/compressor-dashboard/charts/range-chart.js
+++ express-dashboard-server/public/javascripts/compressor-dashboard/charts/range-chart.js
@@ -5,12 +5,18 @@ export class RangeChart{
     this.chart = dc.barChart(this.container_id)
     this.group = dimension.group().reduce(reduceAddAvg(attr), reduceRemoveAvg(attr), reduceInitAvg);
 
+    var y_range = [
+      Math.min.apply(Math, this.group.all().map(function(d) { return d.value.count ? d.value.sum / d.value.count : 0 })) * 0.90,
+      Math.max.apply(Math, this.group.all().map(function(d) { return d.value.count ? d.value.sum / d.value.count : 0 })) * 1.10
+    ];
+    
     this.chart
       .width($(this.container_id).width())
       .height(70)
       .dimension(dimension)
       .group(this.group)
       .x(d3.scaleTime().domain([start, end]))
+      .y(d3.scaleLinear().domain(y_range))
       .centerBar(true)
       .valueAccessor(d => {return d.value.count ? d.value.sum / d.value.count : 0 })
       .renderVerticalGridLines(true)
