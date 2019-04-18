86d39eaf5e26e41a0cbe1df48f57979d2750b32c
evelynbankell <evelyn@bankell.se>
2019-04-18 15:14:59 +0200

    updated avg in compressor-id

---
+++ express-dashboard-server/public/javascripts/compressor-dashboard/displays/avg-oil-pressure-display.js
@@ -0,0 +1,19 @@
+import { reduceAddAvg, reduceRemoveAvg, reduceInitAvg } from '../../avg-reduce.js';
+
+export class AvgOilPressureDisplay {
+
+	constructor(cross_filter, container_id, avg_id) {
+		this.container_id = container_id;
+		this.display = dc.numberDisplay(this.container_id);
+
+    this.group = cross_filter.groupAll().reduce(reduceAddAvg("oil_pressure"), reduceRemoveAvg("oil_pressure"), reduceInitAvg);
+
+    this.display
+      .formatNumber(d3.format(".2f"))
+      .valueAccessor(d => { console.log(d); return d.count ? (d.sum / ((d.count))) : 0 })
+      .html({some: "<h4 class=" + avg_id + "><br>Average Oil Pressure</h4><h5 class=" + avg_id + ">%number bar</h5>"})
+      .group(this.group);
+
+		this.display.render();
+	}
+}
