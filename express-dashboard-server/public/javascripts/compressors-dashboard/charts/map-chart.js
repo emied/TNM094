9014e3691ad3bb094bdb8982a67eeef4a59da701
Linus Mossberg <linmo400@student.liu.se>
2019-04-18 12:09:26 +0200

    Add leaflet map with markers

--- express-dashboard-server/public/javascripts/compressors-dashboard/charts/map-chart.js
+++ express-dashboard-server/public/javascripts/compressors-dashboard/charts/map-chart.js
@@ -2,6 +2,21 @@ export class MapChart
 {
 	constructor(cross_filter, container_id, height, map_data)
 	{
+		this.container_id = container_id;
+		this.dimension = cross_filter.dimension(function(d) { return d.lon + ',' + d.lat; });
+		this.group = this.dimension.group().reduceCount();
 
+		this.chart = dc_leaflet.markerChart(this.container_id)
+			.dimension(this.dimension)
+			.group(this.group)
+			.valueAccessor(d => d.value)
+			.width($(this.container_id).width())
+			.height(height)
+			.center([42.69,25.42])
+			.zoom(7)
+			.renderPopup(false)
+			.filterByArea(true)
+
+		this.chart.render()
 	}
 }
