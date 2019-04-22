241f9f98a5f4304dc2f9b1d481c1e3d46b2a66b1
Linus Mossberg <linmo400@student.liu.se>
2019-04-22 20:48:17 +0200

    Open compressor id page when map marker is clicked

--- express-dashboard-server/public/javascripts/compressors-dashboard/charts/map-chart-cluster.js
+++ express-dashboard-server/public/javascripts/compressors-dashboard/charts/map-chart-cluster.js
@@ -3,23 +3,38 @@ export class MapChartCluster
 	constructor(cross_filter, container_id, height)
 	{
 		this.container_id = container_id;
-		this.dimension = cross_filter.dimension(function(d) { return d.lon + ',' + d.lat; });
-		this.group = this.dimension.group().reduceCount();
 		this.height = height;
+		this.dimension = cross_filter.dimension(function(d) { return d.lon + ',' + d.lat; });
+
+		// Using dc.js / crossfilter in this instance doesn't make much sense. 
+		// Better to just use leaflet directly, but we might need dc.leaflet for choropleth etc. later.
+		this.group = this.dimension.group().reduce(
+			(p,v) => { p.id = v.id; return p; },
+			(p,v) => { p.id = v.id; return p; },
+			() => { return { id: -1 } }
+		);
+
+		function compressorView(e) {
+			window.location.href = "dashboard?data=compressor&view=compressor-id&id=" + e.target.compressor_id;
+		}
 
 		this.chart = dc_leaflet.markerChart(this.container_id)
 			.mapOptions({ zoomSnap: 0.1 }) 
 			.dimension(this.dimension)
 			.group(this.group)
-			.valueAccessor(d => d.value)
+			.valueAccessor(d => d.value.id)
 			.width($(this.container_id).width())
 			.height(this.height)
 			.center([63,18])
 			.zoom(4.6)
-			.popup(function(kv) {
-				return kv.key;
+			.renderPopup(false)
+			.marker((d,map) => {
+				var marker = new L.Marker(this.chart.toLocArray(this.chart.locationAccessor()(d))).on('click', compressorView);
+				marker.compressor_id = d.value.id;
+				return marker;
 			})
 			.cluster(true);
+
 
 		this.chart.render()
 	}
