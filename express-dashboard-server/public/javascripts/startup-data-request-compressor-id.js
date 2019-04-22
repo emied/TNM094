49bb197101ded5d1140b7d6b8b9ddb41ba22d3c3
Linus Mossberg <linmo400@student.liu.se>
2019-04-22 13:54:02 +0200

    Request ID based on URL

--- express-dashboard-server/public/javascripts/startup-data-request-compressor-id.js
+++ express-dashboard-server/public/javascripts/startup-data-request-compressor-id.js
@@ -2,10 +2,14 @@
 import { CompressorIdDashboard } from './compressor-dashboard/compressor-id-dashboard.js';
 
 function startupDataRequest() {
-	var id = 100; 
 	var start = "2018-03-20";
 	var end = "2020-03-28";
 	var decimate = 1;
+
+	// Get id from url
+	var id = new URLSearchParams(window.location.search).get('id');
+	
+	id = id ? id : 0;
 
 	d3.json('api/get_compressor?id=' + id + '&start=' + start + '&end=' + end + '&decimate=' + decimate).then( data => {
 		var compressor_id_dashboard = new CompressorIdDashboard(data);
